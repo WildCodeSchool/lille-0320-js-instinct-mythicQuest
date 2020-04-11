02e2f595af6c837da5364f68551fd039af6dfc2d
LoicBrassart <LoicBrassart@users.noreply.github.com>
2020-04-11 15:53:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Modèle de P2 pour la Wild Code School, cursus React/Node
+
+- CRA
+- Nettoyage des fichiers initiaux
+- Installation de `nose-sass`
+- Mise en place d'un composant-modèle (à supprimer)
+- Remise à zéro du README.md
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wilder from "./components/Wilder";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi there !</h1>
+      <Wilder />
+    </div>
+  );
+}
+
+export default App;
