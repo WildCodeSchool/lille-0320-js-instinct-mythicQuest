c9f014b35f6e938b9fc738e3e98dfc79a6d2957f
magb7 <margaux.bedu@gmail.com>
2020-04-15 12:00:47 +0200

    add paragraph in About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,17 +1,21 @@
 import React from "react";
+import "./styles/AboutPage.scss";
 
 const AboutPage = () => {
-    return (
-      <div className="about">
-        <ul className="name_list">
-          <li>Malika BADAOUI</li>
-          <li>Margaux BEDU</li>
-          <li>Aurélien BADRE</li>
-          <li>Maxime RAWINSKI</li>
-          <li>Sylvain CARLIER</li>
-        </ul>
-      </div>
-    );
+  return (
+    <div className="about">
+      <p>
+        Projet réalisé dans le cadre de la formation à la WildCodeSchool.
+      </p>
+      <ul className="name_list">
+        <li>Malika BADAOUI</li>
+        <li>Margaux BEDU</li>
+        <li>Aurélien BADRE</li>
+        <li>Maxime RAWINSKI</li>
+        <li>Sylvain CARLIER</li>
+      </ul>
+    </div>
+  );
 };
 
 export default AboutPage;
