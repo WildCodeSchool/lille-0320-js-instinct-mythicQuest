bda9bcbba8bdff825f6605f615737997aed5deca
Maxime <maxime.rawinski@gmail.com>
2020-04-15 15:22:23 +0200

    add components Howtoplay

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Howtoplay from "./components/HowToPlay/Howtoplay";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Howtoplay />
+      </main>
     </div>
   );
 }
