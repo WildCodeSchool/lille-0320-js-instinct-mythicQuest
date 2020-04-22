bbce5893a6e433bed162a94bbfb22e5be60a968d
MalikaBadaoui <malikamyriambadaoui@gmail.com>
2020-04-22 15:04:59 +0200

    add Animated Background with Particles.js Library

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Form from "../Form/Form";
 import Nav from "../Share/Nav";
+import Particles from "react-particles-js";
+import ParticlesConfig from "./ParticlesConfig";
 import "./Header.scss";
+import "./ParticlesConfig.scss";
 
 const Header = () => {
   return (
     <header className="homepage-header">
+      <Particles className="particles" params={ParticlesConfig} />
       <Nav />
       <div className="container-content">
         <h1>
