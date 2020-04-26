746be7b5ad7f01f55a4ab4bb93b7386e7ea7a554
magb7 <margaux.bedu@gmail.com>
2020-04-26 18:39:23 +0200

    add constant for sprite source and coins size and source

--- src/components/Constants/Constants.js
+++ src/components/Constants/Constants.js
@@ -1,1 +1,5 @@
 export const sprite_size = 40;
+export const sprite_source = "/game/sprites/player_walk.png";
+
+export const coins_source = "/game/objects/coin_gold.png";
+export const coins_size = 32;
