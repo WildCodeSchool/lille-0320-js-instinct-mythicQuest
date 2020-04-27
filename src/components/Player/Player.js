af77999283fb7e7ac7eb2cab562ab37b2323087e
magb7 <margaux.bedu@gmail.com>
2020-04-27 15:07:20 +0200

    add shift position function

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
+import { sprite_source, sprite_size } from "../Constants/Constants";
 import "./Player.scss";
 
 class Player extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sprite: "/game/sprites/player_walk.png",
+      direction: "DOWN",
+      walkIndex: 0,
+      spriteLocationX: "0px",
+      spriteLocationY: "0px",
     };
   }
+
+  componentDidUpdate() {}
+
+  getSpriteLocation = () => {
+    if (this.props.direction !== this.state.direction) {
+      switch (this.props.direction) {
+        case "UP":
+          this.setState({
+            spriteLocationY: `${sprite_size * 3}`,
+            // spriteLocationX: `${sprite_size * walkIndex}`,
+          });
+          break;
+        case "DOWN":
+          this.setState({
+            spriteLocationY: `${sprite_size * 0}`,
+            // spriteLocationX: `${sprite_size * walkIndex}`,
+          });
+          break;
+        case "LEFT":
+          this.setState({
+            spriteLocationY: `${sprite_size * 2}`,
+            // spriteLocationX: `${sprite_size * walkIndex}`,
+          });
+          break;
+        case "RIGHT":
+          this.setState({
+            spriteLocationY: `${sprite_size * 1}`,
+            // spriteLocationX: `${sprite_size * walkIndex}`,
+          });
+          break;
+        default:
+          return;
+      }
+    }
+  };
 
   render() {
     return (
@@ -17,11 +56,13 @@ class Player extends Component {
           position: "relative",
           top: this.props.position[1], // top est sur l'axe y
           left: this.props.position[0], // left est sur l'axe x
-          backgroundImage: `url(${this.state.sprite})`,
-          backgroundPosition: this.props.spriteLocation,
-          transition: "0.3s",
-          width: "40px",
-          height: "40px",
+          backgroundImage: `url(${sprite_source})`,
+          backgroundPositionX: `${this.state.spriteLocationX}px`,
+          backgroundPositionY: `${this.state.spriteLocationY}px`,
+          transitionProperty: "top, left",
+          transitionDuration: "0.3s",
+          width: `${sprite_size}px`,
+          height: `${sprite_size}px`,
         }}
       ></div>
     );
