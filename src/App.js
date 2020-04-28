2a6fa36e3d12265939ccd44886c4fed65231472f
Aurelien <aurelien.badre.dev@gmail.com>
2020-04-29 00:14:42 +0200

    Added some css to the game page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "./components/HomePage/HomePage";
 import "./App.scss";
+import Game from "./components/Game/Game";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Game />
     </div>
   );
 }
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -52,11 +52,13 @@ class Game extends Component {
 
   render() {
     return (
-      <div className="game-area">
-        <Player
-          position={this.state.position}
-          direction={this.state.direction}
-        />
+      <div className="game-container">
+        <div className="game-area">
+          <Player
+            position={this.state.position}
+            direction={this.state.direction}
+          />
+        </div>
       </div>
     );
   }
