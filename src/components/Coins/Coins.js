ffc17166c551d523419d004798798d201f010fca
magb7 <margaux.bedu@gmail.com>
2020-05-04 11:31:47 +0200

    fixed the teleportation of the coins problem

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { coins_source, coins_size } from "../Constants/Constants";
 
 const Coins = () => {
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  useEffect(() => {
+    setX(getRandomNum());
+    setY(getRandomNum());
+  }, []);
+
   const getRandomNum = () => {
     const min = 1;
     const max = 570;
     let num = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
     return num;
   };
-
-  let x = getRandomNum();
-  let y = getRandomNum();
 
   return (
     <div
@@ -19,9 +24,7 @@ const Coins = () => {
         top: y,
         left: x,
         backgroundImage: `url(${coins_source})`,
-        backgroundPosition: `${
-          coins_size * (Math.floor(Math.random() * 7) + 0)
-        }px 0`,
+        backgroundPosition: `${coins_size * 4}px 0`,
         width: `${coins_size}px`,
         height: `${coins_size}px`,
       }}
