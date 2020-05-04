372a1bc836ea639cc6a531234ef30f940c7b5085
Aurelien <aurelien.badre.dev@gmail.com>
2020-05-04 16:10:15 +0200

    added some architecture to the GamePage

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -5,7 +5,21 @@ import "./GamePage.scss";
 const GamePage = () => {
   return (
     <div className="game-container">
+      <div className="Left-section">
+        <div className="LifeCounter">
+          <p>Life Counter</p>
+        </div>
+        <div className="Stuff">
+          <p>Stuff</p>
+        </div>
+        <div className="Divers">
+          <p>À préciser</p>
+        </div>
+      </div>
       <Game />
+      <div className="Right-section">
+        <p>Score</p>
+      </div>
     </div>
   );
 };
