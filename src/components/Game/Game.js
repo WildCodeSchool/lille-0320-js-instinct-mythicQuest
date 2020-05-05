5a1522b292dac2a99d67c4a1a1dd480285090249
magb7 <margaux.bedu@gmail.com>
2020-05-05 10:54:17 +0200

    add function to prevent wall collision

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,6 +8,7 @@ const initialState = {
   direction: "DOWN",
   positionX: 0,
   positionY: 0,
+  canMove: true,
 };
 
 class Game extends Component {
@@ -17,43 +18,82 @@ class Game extends Component {
   }
 
   componentDidMount() {
-    document.onkeydown = this.onKeyDown;
+    window.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate() {
+    this.getMovement(this.state.direction);
   }
 
   // Event Listener when pressing arrow keys and moving character accordingly
   onKeyDown = (e) => {
     e.preventDefault();
-    const oldPositionX = this.state.positionX;
-    const oldPositionY = this.state.positionY;
     const direction = e.code.replace("Arrow", "").toUpperCase();
     switch (direction) {
       case "UP":
         this.setState({
           direction,
-          positionY: oldPositionY - sprite_size,
         });
         break;
       case "DOWN":
         this.setState({
           direction,
-          positionY: oldPositionY + sprite_size,
         });
         break;
       case "LEFT":
         this.setState({
           direction,
-          positionX: oldPositionX - sprite_size,
         });
         break;
       case "RIGHT":
         this.setState({
           direction,
-          positionX: oldPositionX + sprite_size,
         });
         break;
       default:
         return;
     }
+  };
+
+  getMovement = (direction) => {
+    const oldPositionX = this.state.positionX;
+    const oldPositionY = this.state.positionY;
+    let newPositionX;
+    let newPositionY;
+    switch (direction) {
+      case "UP":
+        newPositionY = oldPositionY - sprite_size;
+        break;
+      case "DOWN":
+        newPositionY = oldPositionY + sprite_size;
+        break;
+      case "LEFT":
+        newPositionX = oldPositionX - sprite_size;
+        break;
+      case "RIGHT":
+        newPositionX = oldPositionX + sprite_size;
+        break;
+      default:
+        return;
+    }
+
+    if (this.isMovePossible(newPositionX, newPositionY)) {
+      return this.setState({
+        positionX: newPositionX,
+        positionY: newPositionY,
+      });
+    }
+  };
+
+  isMovePossible = (x, y) => {
+    const min_x = 0;
+    const min_y = 0;
+    const max_x = 760;
+    const max_y = 560;
+
+    if (x <= min_x || x >= max_x || y <= min_y || y >= max_y) {
+      return false;
+    } else return true;
   };
 
   render() {
