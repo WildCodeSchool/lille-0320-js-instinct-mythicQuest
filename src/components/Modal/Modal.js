6c1b6cfe2942f2ca950e15bab622aa8927f8aaef
magb7 <margaux.bedu@gmail.com>
2020-05-13 16:31:44 +0200

    change onClick function for try again button

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Modal.scss";
 
-const refreshPage = () => {
-  window.location.reload();
-};
-
-const Modal = ({ showModal }) => {
+const Modal = ({ showModal, resetGame }) => {
   return (
     showModal && (
       <div className="ModalBackground">
         <div className="ModalContainer">
           <img src="images/gameover.gif" alt="game over" />
           <div className="button-container">
-            <button onClick={refreshPage} className="restart">
+            <button onClick={resetGame} className="restart">
               Try again
             </button>
             <Link to={"/"}>
