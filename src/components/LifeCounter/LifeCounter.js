8544c9619e50016dd50cf5b7ab421f106d7e3382
magb7 <margaux.bedu@gmail.com>
2020-05-13 11:14:07 +0200

    resolve popup issue

--- src/components/LifeCounter/LifeCounter.js
+++ src/components/LifeCounter/LifeCounter.js
@@ -3,11 +3,10 @@ import Modal from "../Modal/Modal";
 import "./LifeCounter.scss";
 
 const LifeCounter = ({ lifeCounter }) => {
-  const [count, setCount] = useState(3);
   const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
-    if (count === 0) {
+    if (lifeCounter === 0) {
       setTimeout(() => {
         const audio = new Audio("sound/dundundun.mp3");
         audio.volume = 0.4;
@@ -15,21 +14,7 @@ const LifeCounter = ({ lifeCounter }) => {
         showModal();
       }, 400);
     }
-  }, [count]);
-
-  // Code commenté provisoire, sera intégré dans dans une future US
-
-  // const handleClickLose = () => {
-  //   if (count > 0) setCount(count - 1);
-  //   const audio = new Audio("sound/lost-life.wav");
-  //   audio.play();
-  // };
-
-  // const handleClickWin = () => {
-  //   if (count < 3) setCount(count + 1);
-  //   const audio = new Audio("sound/win-life.wav");
-  //   audio.play();
-  // };
+  }, [lifeCounter]);
 
   const showModal = () => {
     setOpenModal(true);
