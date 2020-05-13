a377deddaf108c1b3597f395218624bbdd9e4815
magb7 <margaux.bedu@gmail.com>
2020-05-13 11:05:04 +0200

    add audio constants

--- src/components/Constants/Constants.js
+++ src/components/Constants/Constants.js
@@ -4,3 +4,6 @@ export const sprite_source = "/game/sprites/player_walk.png";
 export const goldCoin_source = "/game/objects/gold.gif";
 export const silverCoin_source = "/game/objects/silver.gif";
 export const coins_size = 30;
+
+export const audioWin = new Audio("sound/win-life.wav");
+export const audioLose = new Audio("sound/lost-life.wav");
